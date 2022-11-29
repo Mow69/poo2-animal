770498c98e666bad940973a44ee186e2111ad427
Mow <mowster69@outlook.com>
2022-11-29 10:05:49 +0100

    Initial Commit

---
+++ TP2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TP2
+//
+//  Created by Mouaz Saadaoui on 29/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
